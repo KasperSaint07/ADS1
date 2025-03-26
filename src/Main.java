95b0484932fbbef4c37e32365ecc5d42e073d7ad
KasperSaint07 <123306320+KasperSaint07@users.noreply.github.com>
2025-03-26 15:50:33 +0500

    daw

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        Problem 1.
+//        You are given a number “n” and an array of “n” elements,
+//                write the function that returns minimum of them.
+
+
+
+    }
+}
